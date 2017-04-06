7a9d759e816ba60583e490eb0f838ce2d41143a3
Saumya Kanoria <svk@breakingwavesoftware.com>
2017-04-06 15:46:37 +0530

    started work on integrating React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-
-import theme from '../assets/react-toolbox/theme';
-import '../assets/react-toolbox/theme.css';
-import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import AppBar from 'react-toolbox/lib/app_bar/AppBar';
 import Layout from 'react-toolbox/lib/layout/Layout';
+import List from 'react-toolbox/lib/list/List';
+import ListItem from 'react-toolbox/lib/list/ListItem';
 import NavDrawer from 'react-toolbox/lib/layout/NavDrawer';
 import Panel from 'react-toolbox/lib/layout/Panel';
-import DatePicker from 'react-toolbox/lib/date_picker/DatePicker';
+
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import theme from '../assets/react-toolbox/theme';
+import '../assets/react-toolbox/theme.css';
+
+import Home from './components/Home';
+import Posts from './components/Posts';
 
 import { apiEndpoint } from './ApiUtils';
 import './index.css';
 
 class App extends Component {
+  state = {
+    navDrawerActive: false
+  };
+
+  toggleNavDrawer = () => {
+    this.setState({ navDrawerActive: !this.state.navDrawerActive });
+  };
+
   componentDidMount() {
     fetch(apiEndpoint('/posts')).then((response) => {
       return response.json();
@@ -24,21 +37,30 @@ class App extends Component {
 
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <NavDrawer>
-            <p>Navigation, account switcher, etc. go here.</p>
-          </NavDrawer>
-          <Panel>
-            <AppBar leftIcon='menu'/>
-            <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
-              <h1>Main Content</h1>
-              <p>Main content goes here.</p>
-              <DatePicker/>
-            </div>
-          </Panel>
-        </Layout>
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <NavDrawer active={this.state.navDrawerActive}
+                       permanentAt="xxxl"
+                       onOverlayClick={this.toggleNavDrawer}>
+              <List>
+                <ListItem caption="Home" itemContent={
+                  <Link to="/" onClick={this.toggleNavDrawer}>Home</Link>
+                }/>
+
+                <ListItem caption="Posts" itemContent={
+                  <Link to="/posts" onClick={this.toggleNavDrawer}>Posts</Link>
+                }/>
+              </List>
+            </NavDrawer>
+            <Panel>
+              <AppBar leftIcon='menu' onLeftIconClick={this.toggleNavDrawer}/>
+              <Route exact path="/" component={Home}/>
+              <Route path="/posts" component={Posts}/>
+            </Panel>
+          </Layout>
+        </ThemeProvider>
+      </Router>
     );
   }
 }
